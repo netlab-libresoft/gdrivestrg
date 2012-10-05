51493607c012dddf26dc7f874d7b64afcd8d6a93
Raul Roman <rroman@libresoft.es>
2012-10-05 14:20:20 +0200

    Inserting Gdrive fields in table

--- lib/templates/create_gdrivestrgfields.rb
+++ lib/templates/create_gdrivestrgfields.rb
@@ -3,11 +3,15 @@ class CreateGdrivestrgfields < ActiveRecord::Migration
     add_column :cloudstrgusers, :gdrive_refresh_token, :string 
     add_column :cloudstrgusers, :gdrive_expires_in, :integer 
     add_column :cloudstrgusers, :gdrive_issued_at, :date
+    
+    Cloudstrglist.create :plugin_name => "gdrive"
   end
 
   def down
     remove_column :cloudstrgusers, :gdrive_refresh_token
     remove_column :cloudstrgusers, :gdrive_expires_in
     remove_column :cloudstrgusers, :gdrive_issued_at
+
+    Cloudstrglist.delete_all :plugin_name => "gdrive"
   end
 end
